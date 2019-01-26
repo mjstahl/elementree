634e59e1f5a072dcfb9baeb71b0257af458b9a5b
Mark Stahl <mjstahl@gmail.com>
2019-01-26 09:34:27 -0500

    Handled regex matching of routes. Need to bind to vars still

--- router.js
+++ router.js
@@ -1,19 +1,45 @@
 const { Stated } = require('@mjstahl/stated')
 
-let appState
+let appState, matches = {}
+function createMatch (route) {
+  const vx = '(:\\w+)'
+  const address = new RegExp(vx, 'g')
+  const vars = (route.match(address) || []).map((v) => v.slice(1))
+  matches[route.replace(address, vx)] = { route, vars }
+}
+
+function match (pathname) {
+  // should probably return the whole object here
+  // that way we get the route, and we get the vars
+  // in case there is state we have to update
+  return Object.keys(matches).find(addr => {
+    return new RegExp(addr, 'g').test(pathname)
+  })
+}
+
 function register (routes, state = {}) {
-  Object.keys(routes).forEach(r => {
-    routes[r] = { view: routes[r], value: state }
-    routes.initial = (routes.initial)
-      ? routes.initial
-      : (new RegExp(r).test(window.location.pathname)) ? r : undefined
+  Object.keys(routes).forEach(route => {
+    routes[route] = { view: routes[route], value: state }
+    createMatch(route)
   })
   appState = new Stated(routes)
+  appState.initial = match(window.location.pathname) || '*'
   return appState
 }
 
 function route (address, value) {
-  appState.to(address, Object.assign(appState.value, value))
+  const route = match(address)
+  if (window.location.pathname !== route) {
+    window.history.pushState({}, null, route)
+  }
+  // once we find the correct route, we need to match
+  // it and relate the matched values to the variables
+  // that are held by <match>.vars
+  try {
+    appState.to(route, Object.assign(appState.value, value))
+  } catch (e) {
+    console.error(`"${address}" does not match a configured route.`)
+  }
 }
 
 function view (routes) {
