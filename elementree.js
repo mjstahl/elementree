8f4f8228aa788808d589080018011ca8d4ed80ab
Mark Stahl <mjstahl@gmail.com>
2019-05-22 13:54:49 -0400

    Fixes #14. Different component instances have different state instances

--- elementree.js
+++ elementree.js
@@ -1,37 +1,68 @@
-const __merge = require('nanomorph')
+const clone = require('lodash.clonedeep')
 const onChange = require('on-change')
+const __merge = require('nanomorph')
+const __renderTemplate = require('nanohtml')
+
 const ready = require('./ready')
 
+const exprCache = new WeakMap()
 let rendering = false
 let root = null
 let tree = null
 
-function __renderer () {
+function __renderAll () {
   if (rendering) { return }
   rendering = true
   rendering = !__merge(root, tree())
 }
 
 function merge (selector, prepared, app = {}) {
-  const model = onChange(app, __renderer)
-  tree = () => prepared(model)
+  const appTemplate = prepared(onChange(app, __renderAll))
+  const model = (appTemplate.initWith)
+    ? onChange(appTemplate.initWith, __renderAll)
+    : undefined
+  tree = () => appTemplate(model)
   ready(() => {
     root = (typeof selector === 'string')
       ? document.querySelector(selector)
       : selector
-    __renderer()
+    __renderAll()
   })
 }
 
 function join (template, state) {
-  if (!state) return template
-  const model = onChange(state, __renderer)
-  return (...args) => template(model, ...args)
+  return (...args) => {
+    function callWithModel (model) {
+      return (model)
+        ? template(model, ...args)
+        : template(...args)
+    }
+    callWithModel.initWith = state
+    return callWithModel
+  }
+}
+
+function render (strings, ...exprs) {
+  let values = exprCache.get(strings)
+  if (!values) {
+    values = exprs.map(e => {
+      return (e && e.name === 'callWithModel')
+        ? onChange(clone(e.initWith), __renderAll)
+        : e
+    })
+    exprCache.set(strings, values)
+  }
+  const evaluated = exprs.map((e, i) => {
+    return (e && e.name === 'callWithModel')
+      ? e(values[i])
+      : e
+  })
+  return __renderTemplate(strings, ...evaluated)
 }
 
 module.exports = {
   html: require('nanohtml/raw'),
   join,
   merge,
-  render: require('nanohtml')
+  render,
 }
