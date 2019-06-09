aa875e6b85a41ff48ebf5e3bcc0ae69c32f95f44
Mark Stahl <mjstahl@gmail.com>
2019-06-09 13:27:20 -0400

    Update type signatures

--- index.d.ts
+++ index.d.ts
@@ -3,9 +3,8 @@ type AppState = Model | object
 
 type CallableWithModel = (state?: Model) => HTMLElement
 type Prepared = (...args: any[]) => CallableWithModel
-type Renderer = (strings: TemplateStringsArray, ...keys: any[]) => HTMLElement
 
-export type html = Renderer
-export type render = Renderer
+export function html(strings: TemplateStringsArray, ...keys: any[]): HTMLElement
+export function render(strings: TemplateStringsArray, ...keys: any[]): HTMLElement
 export function merge(selector: string, prepared: Prepared, state?: AppState): string | void
 export function prepare(template: Function, state?: Model): Prepared
